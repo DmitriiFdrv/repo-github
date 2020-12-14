5f38222f5bbefaca05a64a913d51d85db34b922e
Dmitrii_F <fdorov_dima@mail.ru>
2020-12-14 15:12:26 +0500

    w

---
+++ task_1.1.py
@@ -0,0 +1,11 @@
+a = int(input('Введите число a:'))
+b = int(input('Введите число b:'))
+c = a * b
+print(c)
+
+a = 13
+b = int(input('Введите число b:'))
+c = a / b
+print(c)
+c = a // b
+print(c)
---
+++ task_1.3.py
@@ -0,0 +1,6 @@
+n = int(input("введите целое число:"))
+a = str(n)
+b = a + a
+c = a + a + a
+d = n + int(b) + int(c)
+print(d)
